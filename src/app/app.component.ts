30a61e789ebef214d1fc006ee3c58a4d6da4f2ca
CaioOliveiraG2 <101058407+CaioOliveiraG2@users.noreply.github.com>
2022-09-02 20:36:53 -0300

    Interpolação com method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'Caio Oliveira';
+  nascimento = 1999;
+  counter = 0;
+
+  timer = setInterval(() => {
+    this.counter = this.counter + 1;
+  }, 100);
+
+  getIdade() {
+    return 2022 - this.nascimento;
+  }
+  getMinutos() {
+    return Math.trunc(this.counter / 60);
+  }
+  getSegundos() {
+    return Math.trunc(this.counter % 60);
+  }
 }
